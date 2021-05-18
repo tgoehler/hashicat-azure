32db4b2d8a5f2d21595bd9dcf158a0f5fcb0aef1
torsten goehler <tech@goehler-online.de>
2021-05-18 15:02:27 +0000

    adding remote backend

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
-}
+  }
 
 variable "location" {
   description = "The region where the virtual network is created."
